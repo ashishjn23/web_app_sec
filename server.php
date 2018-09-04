17aaa4330c7c9ec456d6cd7e8f9b4e8bb728be55
Ashish Nimonkar <ashishjn@udel.edu>
2018-09-04 17:33:17 +0000

    Added my first website

--- server.php
+++ server.php
@@ -2,12 +2,12 @@
     //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
- 
+    
     //this is a sample query which gets some data, the order by part shuffles the results
     //the limit 0, 10 takes the first 10 results.
     // you might want to consider taking more results, implementing "pagination", 
     // ordering by rank, etc.
-    $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 10";
+    $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 5";
     
     //this next line could actually be used to provide user_given input to the query to 
     //avoid SQL injection attacks
@@ -18,7 +18,7 @@
     //there are several ways of getting the data out, iterating row by row,
     //I chose to get associative arrays inside of a big array
     //this will naturally create a pleasant array of JSON data when I echo in a couple lines
-    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $results = $statement->fetchAll();
     
     //this part is perhaps overkill but I wanted to set the HTTP headers and status code
     //making to this line means everything was great with this request
